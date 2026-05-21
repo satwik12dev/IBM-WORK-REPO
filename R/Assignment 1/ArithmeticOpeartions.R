0e220f7edc353d54798b1d5e18a5c17383447094
Satwik Saxena <satwiksaxena41@gmail.com>
2026-05-21 22:26:38 +0530

    Fix formatting issues in ArithmeticOperations.R

--- R/Assignment 1/ArithmeticOpeartions.R
+++ R/Assignment 1/ArithmeticOpeartions.R
@@ -1,14 +1,11 @@
-# Define two numbers
 num1 <- 10
 num2 <- 5
 
-# Perform operations
 add <- num1 + num2
 sub <- num1 - num2
 mul <- num1 * num2
 div <- num1 / num2
 
-# Display results
 cat("Addition =", add, "\n")
 cat("Subtraction =", sub, "\n")
 cat("Multiplication =", mul, "\n")
